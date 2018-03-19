5259b0a93b5dce96a9a60cff025087387c0787ad
Morgan Radic <radic44morgan@gmail.com>
2018-03-19 12:37:03 -0400

    added main method

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GreetingController {
+
+    
+    @GetMapping("/address")
+    public String addressForm(Model model) {
+        
+        return "address";
+    }
+
+
+}
