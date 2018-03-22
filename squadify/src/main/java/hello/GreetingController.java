b60f62cb91a4fea785a5a8e2cd780a2b66a2c34b
Morgan Radic <radic44morgan@gmail.com>
2018-03-21 21:44:31 -0400

    user creation with correct failed code messages

--- squadify/src/main/java/hello/GreetingController.java
+++ squadify/src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +10,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class GreetingController {
 
+    private QueueController controller = new QueueController();
+    private ArrayList<UserModel> users = new ArrayList<UserModel>();
     
-    @GetMapping("/u")
-    public String userForm(Model model) {
-        model.addAttribute("user", new UserModel());
-        return "user";
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        model.addAttribute("usermodel", new UserModel());
+        return "login";
+    }
+    
+    @PostMapping("/login")
+    public String loginSubmit(@ModelAttribute UserModel usermodel, Model model)
+    {
+        model.addAttribute("username", usermodel.getUsername());
+        if (controller.addUserToQueue(usermodel, usermodel.getQueuecode()))
+        {
+            return "success";
+        }
+        else
+        {
+            return "failure";
+        }
     }
 
 
